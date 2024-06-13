77ccbc59ed034f3aec5047158d2e7103486bd7c5
Daniel Trejo <dwtrejo14@comcast.net>
2024-06-13 17:30:43 -0400

    added fetch

--- minecraft-project/src/App.jsx
+++ minecraft-project/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import NavBar from './components/NavBar'
 import { Outlet } from 'react-router-dom'
+import { baseUrl } from './components/Global'
 function App() {
+  const [mobData, setMobData] = useState()
+  useEffect(() => {
+    async function getMobData() {
+      const resp = await fetch(baseUrl + 'Mobs')
+      const data = await resp.json()
+      console.log(data)
+      setMobData(data)
+    }
+    getMobData()
+    console.log(mobData)
+  },[])
   return (
     <div>
     <NavBar />
