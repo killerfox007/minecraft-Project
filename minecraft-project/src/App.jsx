a419a001bc17ac198c83bc5f75bb18a8805d80e0
Kaleb Fox <kalebfox7@yahoo.com>
2024-06-14 00:48:45 -0400

    Added Json data and made it display to make sure it worked

--- minecraft-project/src/App.jsx
+++ minecraft-project/src/App.jsx
@@ -3,21 +3,31 @@ import NavBar from './components/NavBar'
 import { Outlet } from 'react-router-dom'
 import { baseUrl } from './components/Global'
 function App() {
-  const [mobData, setMobData] = useState()
+  const [mobData, setMobData] = useState([])
   useEffect(() => {
     async function getMobData() {
       const resp = await fetch(baseUrl + 'Mobs')
       const data = await resp.json()
-      console.log(data)
+      
       setMobData(data)
     }
     getMobData()
-    console.log(mobData)
   },[])
+  
+
+  const mob = mobData.map((singleMob, index) => 
+    <div key={index}>
+    <img src={singleMob.Image} />
+    <p>{singleMob.name}</p>
+    <p>{singleMob.Difficulty}</p>
+    <p>{singleMob.Health}</p>
+    <p>{singleMob.Damage}</p>
+    </div>)
+
   return (
     <div>
     <NavBar />
-    
+    {mob}
     <Outlet />
     </div>
   )
